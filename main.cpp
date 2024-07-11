2d2e7fc4ad8632047a76d30504bf813c7f9971a0
kuriharakento <146906405+kuriharakento@users.noreply.github.com>
2024-07-11 12:05:00 +0900

    プッシュ

--- main.cpp
+++ main.cpp
@@ -1,4 +1,6 @@
+#include <imgui.h>
 #include <Novice.h>
+#include <MyFunctions.h>
 
 const char kWindowTitle[] = "LC1A_13_クリハラ_ケント_タイトル";
 
@@ -7,6 +9,28 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 
 	// ライブラリの初期化
 	Novice::Initialize(kWindowTitle, 1280, 720);
+
+	//１つめ
+	AABB aabb1
+	{
+		.min{-0.5f,-0.5f,-0.5f},
+		.max{0.0f,0.0f,0.0f},
+	};
+
+	//２つめ
+	AABB aabb2
+	{
+		.min{0.2f,0.2f,0.2f},
+		.max{1.0f,1.0f,1.0f},
+	};
+
+	uint32_t color1 = WHITE;
+	uint32_t color2 = WHITE;
+
+	Vector3 cameraTranslate{ 0.0f,1.9f,-6.49f };
+	Vector3 cameraRotate{ 0.26f,0.0f,0.0f };
+
+	float cameraSpeed = 0.01f;
 
 	// キー入力結果を受け取る箱
 	char keys[256] = {0};
@@ -24,6 +48,62 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 		///
 		/// ↓更新処理ここから
 		///
+		if (keys[DIK_W])
+		{
+			cameraTranslate.z += cameraSpeed;
+		}
+		if (keys[DIK_S])
+		{
+			cameraTranslate.z -= cameraSpeed;
+		}
+		if (keys[DIK_A])
+		{
+			cameraTranslate.x -= cameraSpeed;
+		}
+		if (keys[DIK_D])
+		{
+			cameraTranslate.x += cameraSpeed;
+		}
+		if (keys[DIK_Q])
+		{
+			cameraTranslate.y += cameraSpeed;
+		}
+		if (keys[DIK_E])
+		{
+			cameraTranslate.y -= cameraSpeed;
+		}
+
+		Matrix4x4 cameraMatrix = MakeAffineMatrix({ 1.0f,1.0f,1.0f }, cameraRotate, cameraTranslate);
+		Matrix4x4 viewMatrix = Inverse(cameraMatrix);
+		Matrix4x4 projectionMatrix = MakePerspectiveFovMatrix(0.45f, (float)kWindowWidth / (float)kWindowHeight, 0.1f, 100.0f);
+		Matrix4x4 viewProjectionMatrix = Multiply(viewMatrix, projectionMatrix);
+		Matrix4x4 viewportMatrix = MakeViewportMatrix(0, 0, (float)kWindowWidth, (float)kWindowHeight, 0.0f, 1.0f);
+
+		aabb1.min.x = (std::min)(aabb1.min.x, aabb1.min.x);
+		aabb2.min.x = (std::min)(aabb2.min.x, aabb2.min.x);
+
+		aabb1.min.y = (std::min)(aabb1.min.y, aabb1.min.y);
+		aabb2.min.y = (std::min)(aabb2.min.y, aabb2.min.y);
+
+		aabb1.min.z = (std::min)(aabb1.min.z, aabb1.min.z);
+		aabb2.min.z = (std::min)(aabb2.min.z, aabb2.min.z);
+
+		if (isCollision(aabb1, aabb2))
+		{
+			color1 = RED;
+		} else
+		{
+			color1 = WHITE;
+		}
+
+
+		ImGui::Begin("window");
+		ImGui::DragFloat3("CameraRotate", &cameraRotate.x, 0.01f);
+		ImGui::DragFloat3("aabb1.min", &aabb1.min.x, 0.01f);
+		ImGui::DragFloat3("aabb1.max", &aabb1.max.x, 0.01f);
+		ImGui::DragFloat3("aabb2.min", &aabb2.min.x, 0.01f);
+		ImGui::DragFloat3("aabb2.max", &aabb2.max.x, 0.01f);
+		ImGui::End();
 
 		///
 		/// ↑更新処理ここまで
@@ -32,6 +112,10 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 		///
 		/// ↓描画処理ここから
 		///
+
+		DrawGrid(viewProjectionMatrix, viewportMatrix);
+		DrawAABB(aabb1, viewProjectionMatrix, viewportMatrix, color1);
+		DrawAABB(aabb2, viewProjectionMatrix, viewportMatrix, color2);
 
 		///
 		/// ↑描画処理ここまで
